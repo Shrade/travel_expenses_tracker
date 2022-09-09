23682f609e67752531116a44359a6205e10016dc
Shrade <cheok@myfave.com>
2022-09-09 14:36:56 +0800

    revert

--- database.py
+++ database.py
@@ -1,11 +1,7 @@
 from deta import Deta
-# import streamlit as st
-import os
-from boto.s3.connection import S3Connection
+import streamlit as st
 
-
-DETA_KEY = S3Connection(os.environ['DETA_KEY'])
-# DETA_KEY = st.secrets["DETA_KEY"]
+DETA_KEY = st.secrets["DETA_KEY"]
 
 deta = Deta(DETA_KEY)
 

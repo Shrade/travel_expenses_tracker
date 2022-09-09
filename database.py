a9723680e600b17f7ed18e8f7ae94feb14adf7ac
Shrade <cheok@myfave.com>
2022-09-09 14:35:01 +0800

    push to heroku

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
 from deta import Deta
-import streamlit as st
+# import streamlit as st
+import os
+from boto.s3.connection import S3Connection
 
-DETA_KEY = st.secrets["DETA_KEY"]
+
+DETA_KEY = S3Connection(os.environ['DETA_KEY'])
+# DETA_KEY = st.secrets["DETA_KEY"]
 
 deta = Deta(DETA_KEY)
 

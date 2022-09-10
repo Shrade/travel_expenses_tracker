296219220cede61a4e0c83b2ff5fd8ca3805e5be
cheok1997 <45992088+Shrade@users.noreply.github.com>
2022-09-10 18:14:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,67 +7,77 @@ from forex_python.converter import CurrencyRates
 from streamlit_option_menu import option_menu
 
 import database as db
+import helper
 
-page_title = 'Travel Expenses Tracker'
-page_icon = ':money_with_wings:'
+authenticator = helper.main()
+name, authentication_status, username = authenticator.login('Login', 'main')
 
-st.set_page_config(page_title=page_title, page_icon=page_icon)
-st.title(page_title + " " + page_icon)
+if authentication_status:
+            page_title = 'Travel Expenses Tracker'
+            page_icon = ':money_with_wings:'
 
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+            st.set_page_config(page_title=page_title, page_icon=page_icon)
+            st.title(page_title + " " + page_icon)
 
-years = [datetime.today().year]
+            hide_st_style = """
+                        <style>
+                        #MainMenu {visibility: hidden;}
+                        footer {visibility: hidden;}
+                        header {visibility: hidden;}
+                        </style>
+                        """
+            st.markdown(hide_st_style, unsafe_allow_html=True)
 
-expenses = ["Cost","Tax Amount"]
+            years = [datetime.today().year]
 
-locations = ['San Diego','Los Angeles']
-category = ['Food','Transportation','Accommodation','Internet Plan','Merchandise','Event Ticket','Miscellaneous',]
+            expenses = ["Cost","Tax Amount"]
 
-data = db.fetch_all_data()
+            locations = ['San Diego','Los Angeles']
+            category = ['Food','Transportation','Accommodation','Internet Plan','Merchandise','Event Ticket','Miscellaneous',]
 
-total_expenses = sum([sum(i.get("expenses").values()) for i in data])
+            data = db.fetch_all_data()
 
-c = CurrencyRates()
-conversion_rate = round(c.get_rate('USD','MYR'),3)
+            total_expenses = sum([sum(i.get("expenses").values()) for i in data])
 
-expenses_myr = round(total_expenses*conversion_rate,2)
+            c = CurrencyRates()
+            conversion_rate = round(c.get_rate('USD','MYR'),3)
 
-col1,col2,col3 = st.columns(3)
-col1.metric("Total Expenses(MYR)", f'RM {expenses_myr}')
-col2.metric("Total Expenses(USD)", f'{total_expenses} $')
-col3.metric("Conversion Rate", f'{conversion_rate}')
+            expenses_myr = round(total_expenses*conversion_rate,2)
 
-with st.form("entry_form", clear_on_submit=True):
-    period = str(st.date_input("Date"))
-    col1, col2 = st.columns(2)
-    col1.selectbox("Category:",category, key="category")
-    col2.selectbox("Location:", locations, key="locations")
-    
-    # with st.expander("Tap Here to Input"):
-    item_name = st.text_input("Item Name")
-    for expense in expenses:
-        st.number_input(f'{expense}', min_value=0, format="%i", step=1,key=expense)
-    
-    comment = st.text_area("", placeholder="Enter a comment here")
+            col1,col2,col3 = st.columns(3)
+            col1.metric("Total Expenses(MYR)", f'RM {expenses_myr}')
+            col2.metric("Total Expenses(USD)", f'{total_expenses} $')
+            col3.metric("Conversion Rate", f'{conversion_rate}')
 
-    "---"
+            with st.form("entry_form", clear_on_submit=True):
+                period = str(st.date_input("Date"))
+                col1, col2 = st.columns(2)
+                col1.selectbox("Category:",category, key="category")
+                col2.selectbox("Location:", locations, key="locations")
 
-    submitted = st.form_submit_button("Submit")
+                # with st.expander("Tap Here to Input"):
+                item_name = st.text_input("Item Name")
+                for expense in expenses:
+                    st.number_input(f'{expense}', min_value=0, format="%i", step=1,key=expense)
 
-    if submitted:
+                comment = st.text_area("", placeholder="Enter a comment here")
 
-        category = str(st.session_state["category"])
-        location = str(st.session_state["locations"])
-        expenses = {expense: st.session_state[expense] for expense in expenses}
+                "---"
 
-        db.insert_data(period,category,location,item_name,expenses,comment)
-        st.success("Submitted")
-    
-st.dataframe(pd.DataFrame(data))
+                submitted = st.form_submit_button("Submit")
+
+                if submitted:
+
+                    category = str(st.session_state["category"])
+                    location = str(st.session_state["locations"])
+                    expenses = {expense: st.session_state[expense] for expense in expenses}
+
+                    db.insert_data(period,category,location,item_name,expenses,comment)
+                    st.success("Submitted")
+
+            st.dataframe(pd.DataFrame(data))
+            
+            authenticator.logout('Logout', 'main')
+
+elif authentication_status == False:
+    st.error('Username/password is incorrect')

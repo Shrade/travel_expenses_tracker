d1eadde23eae4c73c445c3024f110fc237708c27
cheok1997 <45992088+Shrade@users.noreply.github.com>
2022-09-10 18:20:06 +0800

    add option menu

--- app.py
+++ app.py
@@ -26,56 +26,68 @@ if authentication_status:
                 </style>
                 """
     st.markdown(hide_st_style, unsafe_allow_html=True)
+    
+    selected = option_menu(
+        menu_title=None,
+        options=["Expenses", "Documents"],
+        icons=["house", "book"],
+        menu_icon="cast", 
+        default_index=0,
+        orientation='horizontal'
+    )
+    
+    if selected == 'Expenses':
+        
+        years = [datetime.today().year]
+        expenses = ["Cost","Tax Amount"]
+        locations = ['San Diego','Los Angeles']
+        category = ['Food','Transportation','Accommodation','Internet Plan','Merchandise','Event Ticket','Miscellaneous',]
 
-    years = [datetime.today().year]
+        data = db.fetch_all_data()
 
-    expenses = ["Cost","Tax Amount"]
+        total_expenses = sum([sum(i.get("expenses").values()) for i in data])
+        c = CurrencyRates()
+        conversion_rate = round(c.get_rate('USD','MYR'),3)
+        expenses_myr = round(total_expenses*conversion_rate,2)
 
-    locations = ['San Diego','Los Angeles']
-    category = ['Food','Transportation','Accommodation','Internet Plan','Merchandise','Event Ticket','Miscellaneous',]
+        col1,col2,col3 = st.columns(3)
+        col1.metric("Total Expenses(MYR)", f'RM {expenses_myr}')
+        col2.metric("Total Expenses(USD)", f'{total_expenses} $')
+        col3.metric("Conversion Rate", f'{conversion_rate}')
 
-    data = db.fetch_all_data()
+        with st.form("entry_form", clear_on_submit=True):
+            period = str(st.date_input("Date"))
+            col1, col2 = st.columns(2)
+            col1.selectbox("Category:",category, key="category")
+            col2.selectbox("Location:", locations, key="locations")
 
-    total_expenses = sum([sum(i.get("expenses").values()) for i in data])
+            # with st.expander("Tap Here to Input"):
+            item_name = st.text_input("Item Name")
+            for expense in expenses:
+                st.number_input(f'{expense}', min_value=0, format="%i", step=1,key=expense)
 
-    c = CurrencyRates()
-    conversion_rate = round(c.get_rate('USD','MYR'),3)
+            comment = st.text_area("", placeholder="Enter a comment here")
 
-    expenses_myr = round(total_expenses*conversion_rate,2)
+            "---"
 
-    col1,col2,col3 = st.columns(3)
-    col1.metric("Total Expenses(MYR)", f'RM {expenses_myr}')
-    col2.metric("Total Expenses(USD)", f'{total_expenses} $')
-    col3.metric("Conversion Rate", f'{conversion_rate}')
+            submitted = st.form_submit_button("Submit")
 
-    with st.form("entry_form", clear_on_submit=True):
-        period = str(st.date_input("Date"))
-        col1, col2 = st.columns(2)
-        col1.selectbox("Category:",category, key="category")
-        col2.selectbox("Location:", locations, key="locations")
+            if submitted:
 
-        # with st.expander("Tap Here to Input"):
-        item_name = st.text_input("Item Name")
-        for expense in expenses:
-            st.number_input(f'{expense}', min_value=0, format="%i", step=1,key=expense)
+                category = str(st.session_state["category"])
+                location = str(st.session_state["locations"])
+                expenses = {expense: st.session_state[expense] for expense in expenses}
 
-        comment = st.text_area("", placeholder="Enter a comment here")
+                db.insert_data(period,category,location,item_name,expenses,comment)
+                st.success("Submitted")
 
-        "---"
-
-        submitted = st.form_submit_button("Submit")
-
-        if submitted:
-
-            category = str(st.session_state["category"])
-            location = str(st.session_state["locations"])
-            expenses = {expense: st.session_state[expense] for expense in expenses}
-
-            db.insert_data(period,category,location,item_name,expenses,comment)
-            st.success("Submitted")
-
-    st.dataframe(pd.DataFrame(data))
-
+        st.dataframe(pd.DataFrame(data))
+    
+    elif selected == 'Documents:
+    
+        link='[Travel Document](https://drive.google.com/drive/u/0/folders/1s1-aMKNh5aY25XdcbPe1h0mta6wDrQGe)'
+        st.markdown(link,unsafe_allow_html=True)
+        
     authenticator.logout('Logout', 'main')  
 
 elif authentication_status == False:
